7090c58435d6357d8a0b07f1e1c11cc6c2e43545
lmunck <anders@lmunck.com>
2020-06-16 19:56:59 +0200

    Initial Commit

---
+++ UniversalLink/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UniversalLink
+//
+//  Created by Anders Munck on 16/06/2020.
+//  Copyright © 2020 Anders Munck. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
